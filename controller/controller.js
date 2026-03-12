f6dad652023ce6bb9bb055b86d090eeebb590643
Beariz Arruda da Silva <art0066413@aluno.firjansenaisesi.com.br>
2026-03-12 15:30:51 -0300

    segundo commit

--- controller/controller.js
+++ controller/controller.js
@@ -1,12 +1,33 @@
 const express = require("express")
 const app = express.Router()
 app.use(express.json())
+const Service = require("../service/service")
+const service = new Service()
 
 // ROTAS
+app.get("/usuario", (req, resp) => {
+    let listaUsuario = service.getUsuarios()
+    resp.json(listaUsuario)
+})
 
+// app.get("/usuario/:id", (res, req) => {
+    
+// })
 
+// // INCLUIR USUÁRIO
+// app.post("/usuario", (res, req) => {
+    
+// })
 
+// // CONSULTAR USUÁRIO
+// app.put("/usuario/:id", (res, req) => {
+    
+// })
 
+// // EXCLUIR USUÁRIO
+// app.delete("/usuario/:id", (res, req) => {
+    
+// })
 
 
 module.exports = app
